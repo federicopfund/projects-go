509413a4db6f328bac04ee10915e3fa5d1aa04b8
Federico Pfund <federicopfund@gmail.com>
2022-03-30 01:41:38 -0300

    router book

---
+++ go-bookstore/pkg/routes/bookstore-routers.go
@@ -0,0 +1,15 @@
+package routes
+
+import(
+	
+	"github.com/gorilla/mux"
+	"github.com/federicopfund/go-bookstore/pkg/controllers"
+
+)
+var RegisterBookStoreRouters = func(router *mux.Router){
+	router.HandleFunc("/book/",controllers.CreateBook).Methods("POTS")
+	router.HandleFunc("/book/",controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}",controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}",controllers.DeleteBook).Methods("DELETE")
+}
